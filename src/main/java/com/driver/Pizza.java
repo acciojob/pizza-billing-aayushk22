0486c6299955beea21c69828a5f23764ffb308eb
aayushk22 <ayushkumar22.ak@gmail.com>
2023-08-11 23:56:34 +0530

    4th Attempt

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -58,9 +58,9 @@ public class Pizza {
     public String getBill(){
         // your code goes here
         if (isVeg) {
-            this.bill += "Base Price Of the Pizza: 300 \n";
+            this.bill += "Base Price Of The Pizza: 300 \n";
         }
-        else this.bill += "Base Price Of the Pizza: 400 \n";
+        else this.bill += "Base Price Of The Pizza: 400 \n";
 
         if (isExtraCheese) {
             this.bill += "Extra Cheese Added: 80 \n";
